bae1a4c67f755438b52d28fbfa65172fdba96177
ciaoSora <657913055@qq.com>
2021-11-27 11:42:19 +0800

    Add a dummy terminal into the website.

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,1 +1,21 @@
-console.log("Hello from javascript")
+const terminalElement = document.getElementById("terminal")
+terminalElement.style.visibility = "visible"
+const term = new Terminal({cursorBlink: true})
+
+// xterm
+term.onData(data => {
+	console.log(data)
+	term.write(data.replaceAll("\r", "\r\n"))
+})
+term.open(terminalElement)
+term.write("> ")
+
+// control button
+function switchVisible() {
+	if (terminalElement.style.visibility === "visible") {
+		terminalElement.style.visibility = "hidden"
+	}
+	else {
+		terminalElement.style.visibility = "visible"
+	}
+}
